4e366a29ad278b37c6a3b8da716b0ff7609c4404
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-10-02 17:50:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here!
-
+const ol=document.getElementById("infi-list");
+for(let i=1;i<=10;i++){
+	createLi(i);
+}
+let count=11;
+ol.addEventListener("scroll",()=>{
+	createLi(count++)
+})
+function createLi(i) {
+	const li=document.createElement("li");
+	li.innerText=`Item ${i}`;
+	ol.appendChild(li);
+}
